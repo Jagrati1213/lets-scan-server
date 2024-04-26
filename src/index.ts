5e61de86b4807f3fad12117141b1b8693cf378ff
Jagrati Gupta <123jagratigupta@gmail.com>
2024-04-26 19:41:31 +0530

    JG: create app listen & data connect

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import express, { Application, Response, Request } from "express";
+import { ConnectionWithMongoDb } from "./db/connection";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 
 const app: Application = express();
-const PORT: number = 3000;
-app.listen(PORT, () => {
-  console.log("server started at Port:", PORT);
+const port = process.env.PORT;
+
+// Middlewares
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+// DB connections
+ConnectionWithMongoDb();
+
+// App started
+app.listen(port, () => {
+  console.log("server started!");
 });
