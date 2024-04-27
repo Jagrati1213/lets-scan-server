fb5ee2d6ea833fef6206498f48097111a5546347
Jagrati Gupta <123jagratigupta@gmail.com>
2024-04-27 23:09:00 +0530

    JG: updates - add middlewares for express app - connect with mongodb - add api routes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Application, Response, Request } from "express";
 import { ConnectionWithMongoDb } from "./db/connection";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import { router } from "./routes/user-route";
 
 dotenv.config();
 
@@ -9,16 +11,38 @@ const app: Application = express();
 const port = process.env.PORT;
 
 // Middlewares
+// Handle cors origin
 app.use(
   cors({
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
 
-// DB connections
-ConnectionWithMongoDb();
+// Handle json as request
+app.use(express.json());
 
-// App started
-app.listen(port, () => {
-  console.log("server started!");
-});
+// Handle url params like (space)
+app.use(express.urlencoded({ extended: true }));
+
+// Handle file upload
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+// router
+app.use("/api", router);
+
+// DB connections
+ConnectionWithMongoDb()
+  .then(() => {
+    // App started
+    app.listen(port, () => {
+      console.log("server started!");
+    });
+  })
+  .catch(() => {
+    console.log("Mongoose not connected");
+  });
+
+export { app };
