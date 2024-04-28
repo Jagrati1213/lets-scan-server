de6d834b9d9438ca04aded161278ff48a372bbb1
Jagrati Gupta <123jagratigupta@gmail.com>
2024-04-28 18:49:48 +0530

    JG; create user route

--- src/routes/user-route.ts
+++ src/routes/user-route.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
 import { handlerSignUp, handlerSignIn } from "../controllers/auth";
+import { handleVerifyToken } from "../controllers/verifyToken";
+import { handleGetUserDetails } from "../controllers/getUserDetails";
 
 const router = express.Router();
 
 router.post("/signup", handlerSignUp);
 router.post("/signin", handlerSignIn);
+router.get("/user", handleVerifyToken, handleGetUserDetails);
 
 export { router };
