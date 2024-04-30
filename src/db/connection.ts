3defb5071a33da4f12e132ed36adc37b793d84ef
Jagrati Gupta <123jagratigupta@gmail.com>
2024-05-01 00:38:01 +0530

    JG; remove variable

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -3,9 +3,7 @@ import { DB_NAME } from "../constants";
 
 export async function ConnectionWithMongoDb() {
   try {
-    const connection = await mongoose.connect(
-      `${process.env.MONGODB_URL}/${DB_NAME}`
-    );
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
     console.log("Mongoose connected!!");
   } catch (error) {
     console.log("Mongoose Connection Error", error);
