5931c8dd7c189b040d3b2d307bae3da74eb30396
Jagrati Gupta <123jagratigupta@gmail.com>
2024-04-30 17:16:40 +0530

    jG: create router for refresh token

--- src/routes/user-route.ts
+++ src/routes/user-route.ts
@@ -2,11 +2,18 @@ import express, { Request, Response } from "express";
 import { handlerSignUp, handlerSignIn } from "../controllers/auth";
 import { handleVerifyToken } from "../controllers/verifyToken";
 import { handleGetUserDetails } from "../controllers/getUserDetails";
+import { handleRefreshToken } from "../controllers/refreshToken";
 
 const router = express.Router();
 
 router.post("/signup", handlerSignUp);
 router.post("/signin", handlerSignIn);
 router.get("/user", handleVerifyToken, handleGetUserDetails);
+router.get(
+  "/refresh",
+  handleRefreshToken,
+  handleVerifyToken,
+  handleGetUserDetails
+);
 
 export { router };
