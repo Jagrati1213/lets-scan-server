2a28c2dd2e789202c68f5377f1a901a6e8046f4a
Jagrati Gupta <123jagratigupta@gmail.com>
2024-05-04 20:31:12 +0530

    JG: return the function

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -1,7 +1,7 @@
 import { Response, Request, NextFunction } from "express";
 
 export const asyncHandler = (requestHandler: any) => {
-  (req: Request, res: Response, next: NextFunction) => {
+  return (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve()
       .then(() => requestHandler(req, res, next))
       .catch((error) => next(error));
