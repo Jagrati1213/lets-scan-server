ff4158333ebb2c6a848f5c070d1825b585167bee
Jagrati Gupta <123jagratigupta@gmail.com>
2024-05-07 23:03:49 +0530

    JG: create login and logout route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,7 +1,14 @@
 import express from "express";
-import { registerUser } from "../controllers/user.controller";
+import {
+  logOutUser,
+  loginUser,
+  registerUser,
+} from "../controllers/user.controller";
+import { authVerify } from "../middlewares/authVerify.middileware";
 
 export const userRouter = express.Router();
 
 // CREATE ROUTER
 userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.post("/logout", authVerify, logOutUser);
