ba02d7a6cca0bb196d680496d9aa55bcb6033f43
Jagrati Gupta <123jagratigupta@gmail.com>
2024-05-07 23:01:56 +0530

    JG: updates - create access & refresh token - save refresh token in db. - create login & logout

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,6 +3,28 @@ import { asyncHandler } from "../utils/asyncHandler";
 import { ApiErrors } from "../utils/apiErrors";
 import { userCollection } from "../models/user.model";
 import { ApiResponse } from "../utils/apiResponse";
+import { Types } from "mongoose";
+import { CustomRequest } from "../types";
+
+export const generateAccessAndRefreshToken = async (
+  userId: Types.ObjectId
+): Promise<{ accessToken: string; refreshToken: string } | undefined> => {
+  try {
+    const user = await userCollection.findOne({ _id: userId });
+    if (!user) return;
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    // SET REFRESH TOKEN IN DB
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    // RETURN TOKEN
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiErrors({ statusCode: 500, message: "CREATED TOKEN ERROR" });
+  }
+};
 
 export const registerUser = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -56,10 +78,82 @@ export const registerUser = asyncHandler(
 export const loginUser = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     // GET USER'S DETAILS
+    const { username, password } = req.body;
+
     // CHECK VALIDATION FOR FIELDS
-    // CONVERT HASH TO PLAIN IN PASSWORD
-    // CREATE TOKEN
+    if (!username || !password) {
+      throw new ApiErrors({
+        statusCode: 401,
+        message: "CREDENTIALS ARE WRONG, TRY AGAIN!",
+      });
+    }
     // CHECK USER'S EXISTENCE
-    // RETURN RESPONSE
+    const exitsUser = await userCollection.findOne({ username });
+    if (!exitsUser || !exitsUser.password)
+      throw new ApiErrors({ statusCode: 400, message: "USER NOT EXITS" });
+
+    // PASSWORD CHECK
+    const passwordCorrect = await exitsUser.isPasswordCorrect(password);
+    if (!passwordCorrect)
+      throw new ApiErrors({ statusCode: 401, message: "INCORRECT PASSWORD!" });
+
+    // CREATE TOKEN
+    const tokens = await generateAccessAndRefreshToken(exitsUser._id);
+    if (!tokens) return;
+
+    const loggedUser = await userCollection
+      .findById(exitsUser?._id)
+      .select("-password -refreshToken");
+
+    // SEND IN COOKIES
+    return res
+      .status(200)
+      .cookie("accessToken", tokens.accessToken, {
+        httpOnly: true,
+        secure: true,
+      })
+      .cookie("refreshToken", tokens.refreshToken, {
+        httpOnly: true,
+        secure: true,
+      })
+      .json(
+        new ApiResponse({
+          statusCode: 200,
+          message: "SUCCESSFULLY LOGIN",
+          data: {
+            user: loggedUser,
+            accessToken: tokens.accessToken,
+            refreshToken: tokens.refreshToken,
+          },
+        })
+      );
+  }
+);
+
+export const logOutUser = asyncHandler(
+  async (req: CustomRequest, res: Response, next: NextFunction) => {
+    await userCollection.findByIdAndUpdate(
+      req.user?._id,
+      {
+        $set: {
+          refreshToken: undefined,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+
+    return res
+      .status(200)
+      .clearCookie("accessToken", { httpOnly: true, secure: true })
+      .clearCookie("refreshToken", { httpOnly: true, secure: true })
+      .json(
+        new ApiResponse({
+          statusCode: 200,
+          message: "LOGOUT SUCCESSFULLY.",
+          data: null,
+        })
+      );
   }
 );
