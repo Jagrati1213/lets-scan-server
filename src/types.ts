f1fc1244b915363e0631055a08b2dea078fb42e0
Jagrati Gupta <123jagratigupta@gmail.com>
2024-05-07 22:48:08 +0530

    JG: create type file for store all types

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+import { Types } from "mongoose";
+import { Request } from "express";
+
+// CREATE INTERFACE FOR USER
+export interface userTypes extends Document {
+  _id: Types.ObjectId;
+  username: string;
+  email: string;
+  password: string;
+  refreshToken: string;
+  menuItems: [];
+  isPasswordCorrect: (password: string) => Promise<boolean>;
+  generateAccessToken: () => string;
+  generateRefreshToken: () => string;
+}
+
+// CREATE INTERFACE FOR CUSTOM REQUEST
+export interface CustomRequest extends Request {
+  user?: userTypes;
+}
