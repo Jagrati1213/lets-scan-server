ed53242835c3bf631e053b110523d0e5059b5938
Jagrati Gupta <123jagratigupta@gmail.com>
2024-05-10 01:02:44 +0530

    JG: create route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -6,6 +6,7 @@ import {
 } from "../controllers/user.controller";
 import { authVerify } from "../middlewares/authVerify.middileware";
 import { refreshTokenCreate } from "../controllers/refreshToken.controller";
+import { getUserDetails } from "../controllers/getUserDetails.controller";
 
 export const userRouter = express.Router();
 
@@ -14,3 +15,4 @@ userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/logout", authVerify, logOutUser);
 userRouter.post("/refresh-token", refreshTokenCreate);
+userRouter.post("/user-details", authVerify, getUserDetails);
