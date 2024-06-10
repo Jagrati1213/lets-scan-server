e58211e16eb13e0a83d2c9c1b68d3469f6e2af3e
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-10 23:03:53 +0530

    JG : Add update and delete handler

--- src/controllers/menu.controller.ts
+++ src/controllers/menu.controller.ts
@@ -9,13 +9,13 @@ import { userCollection } from "../models/user.model";
 
 // CREATE NEW ITEM
 export const createMenuItem = asyncHandler(
-  async (req: CustomRequest, res: Response, next: NextFunction) => {
+  async (req: CustomRequest, res: Response) => {
     try {
       // GET BODY OF MENU ITEM
       const { name, price, desc } = req.body;
 
       // GET USER ID FROM REQ OBJECT
-      const exitsUser = await userCollection
+      const currentUser = await userCollection
         .findById(req.user?._id)
         .select("-password -refreshToken");
 
@@ -24,7 +24,7 @@ export const createMenuItem = asyncHandler(
         return res.json(
           new ApiErrors({
             statusCode: 401,
-            statusText: "ALL FIELDS ARE REQUIRED, TRY AGAIN!",
+            statusText: "ALL FIELDS ARE REQUIRED!",
           })
         );
       }
@@ -53,7 +53,7 @@ export const createMenuItem = asyncHandler(
           image: itemImage,
           price: Number(price),
           rating: 2.5,
-          userId: exitsUser?._id,
+          userId: currentUser?._id,
         });
 
         // CHECK MENU ITEM IS CREATED OR NOT
@@ -72,13 +72,14 @@ export const createMenuItem = asyncHandler(
 
         // PUSH THE ITEMS TO USER DB
         await userCollection.findByIdAndUpdate(
-          { _id: exitsUser?._id },
+          { _id: currentUser?._id },
           {
             $push: {
               menuItems: createdMenuItem?._id,
             },
           }
         );
+
         // SEND RESPONSE OF MENUITEM
         return res.json(
           new ApiResponse({
@@ -104,6 +105,160 @@ export const createMenuItem = asyncHandler(
         })
       );
     }
+  }
+);
+
+// UPDATE MENU ITEM
+export const updateMenuItem = asyncHandler(
+  async (req: CustomRequest, res: Response) => {
+    try {
+      // GET BODY OF MENU ITEM
+      const { name, price, desc, menuId } = req.body;
+
+      // CHECK FIELDS
+      if (!name || !price || !desc) {
+        return res.json(
+          new ApiErrors({
+            statusCode: 400,
+            statusText: `ALL FIELDS REQUIRED!`,
+          })
+        );
+      }
+
+      // CHECK MENU ID EXITS OR NOT
+      const oldMenuItem = await menuCollection.findById(menuId);
+      if (!oldMenuItem) {
+        return res.json(
+          new ApiErrors({
+            statusCode: 400,
+            statusText: "MENU ITEM NOT EXITS!",
+          })
+        );
+      }
+
+      // GET USER ID FROM REQ OBJECT
+      const currentUser = await userCollection
+        .findById(req.user?._id)
+        .select("-password -refreshToken");
+
+      // TODO: FIX REPLICATION IN CLOUD
+
+      // IF WE HAVE IMAGE THEN STORE IN CLOUD
+      if (req.files && "image" in req.files) {
+        const itemImageLocalPath = req.files?.image[0]?.path;
+        const itemImage = await uploadOnCloudinary(itemImageLocalPath);
+
+        // CHECK IMAGE UPLOADED OR NOT
+        if (!itemImage) {
+          return res.json(
+            new ApiErrors({
+              statusCode: 404,
+              statusText: "IMAGE IS NOT UPLOADED IN CLOUD!",
+            })
+          );
+        }
+
+        // UPDATE MENU ITEM IN DB
+        const updatedMenuItem = await menuCollection.findByIdAndUpdate(
+          { _id: menuId },
+          {
+            name: name,
+            description: desc,
+            image: itemImage,
+            price: Number(price),
+          },
+          {
+            new: true,
+          }
+        );
+
+        // CHECK MENU ITEM IS CREATED OR NOT
+        if (!updatedMenuItem) {
+          return res.json(
+            new ApiErrors({
+              statusCode: 409,
+              statusText: "MENU ITEM NOT UPDATED YET, TRY AGAIN!",
+            })
+          );
+        }
+
+        // PUSH THE ITEMS TO USER DB
+        await userCollection.findByIdAndUpdate(
+          { _id: currentUser?._id },
+          {
+            $addToSet: {
+              menuItems: updatedMenuItem?._id, //AVOID REPLICATION
+            },
+          },
+          {
+            new: true,
+          }
+        );
+
+        // SEND RESPONSE OF MENUITEM
+        return res.json(
+          new ApiResponse({
+            statusCode: 201,
+            statusText: "MENU ITEM UPDATED SUCCESSFULLY!",
+            data: updatedMenuItem,
+          })
+        );
+      } else {
+        return res.json(
+          new ApiErrors({
+            statusCode: 404,
+            statusText: "IMAGE IS REQUIRED!",
+          })
+        );
+      }
+    } catch (err) {
+      return res.json(
+        new ApiErrors({
+          statusCode: 400,
+          statusText: `ERROR IN UPDATE MENU ITEM, ${err}`,
+        })
+      );
+    }
+  }
+);
+
+// DELETE MENU ITEM
+export const deleteMenuItem = asyncHandler(
+  async (req: CustomRequest, res: Response) => {
+    // GET MENU ID
+    const { menuId, userId } = req.body;
+
+    // FIND & DELETE MENU ITEM
+    const oldMenuItem = await menuCollection.findByIdAndDelete(menuId);
+
+    if (!oldMenuItem) {
+      return res.json(
+        new ApiErrors({
+          statusCode: 400,
+          statusText: "ITEM NOT DELETED!",
+        })
+      );
+    }
+
+    // DELETE FROM USER COLLECTION
+    await userCollection.findByIdAndUpdate(
+      { _id: userId },
+      {
+        $pull: { menuItems: menuId },
+      },
+      {
+        new: true,
+      }
+    );
+
+    // SEND RESPONSE
+    return res.json(
+      new ApiResponse({
+        statusCode: 200,
+        statusText: "ITEM DELETED SUCCESSFULLY!",
+        data: null,
+      })
+    );
   }
 );
 
