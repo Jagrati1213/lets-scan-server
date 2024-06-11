f3aa2489527174c3c1d10f291433ac5ef079421f
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-11 23:51:44 +0530

    JG: Update in create menu - remove uploading - take image url from frontend

--- src/controllers/menu.controller.ts
+++ src/controllers/menu.controller.ts
@@ -1,8 +1,7 @@
-import { NextFunction, Request, Response } from "express";
+import { Response } from "express";
 import { asyncHandler } from "../utils/asyncHandler";
 import { ApiResponse } from "../utils/apiResponse";
 import { ApiErrors } from "../utils/apiErrors";
-import { uploadOnCloudinary } from "../utils/cloudinary";
 import { menuCollection } from "../models/menu.model";
 import { CustomRequest } from "../types";
 import { userCollection } from "../models/user.model";
@@ -12,7 +11,7 @@ export const createMenuItem = asyncHandler(
   async (req: CustomRequest, res: Response) => {
     try {
       // GET BODY OF MENU ITEM
-      const { name, price, desc } = req.body;
+      const { name, price, desc, image } = req.body;
 
       // GET USER ID FROM REQ OBJECT
       const currentUser = await userCollection
@@ -20,7 +19,7 @@ export const createMenuItem = asyncHandler(
         .select("-password -refreshToken");
 
       // CHECK VALIDATION FOR FIELDS
-      if (!name || !price || !desc) {
+      if (!name || !price || !desc || !image) {
         return res.json(
           new ApiErrors({
             statusCode: 401,
@@ -29,75 +28,50 @@ export const createMenuItem = asyncHandler(
         );
       }
 
-      // HANDLE ERROR FOR REQUEST'S FILE IN IMAGE
-      if (req.files && "image" in req.files) {
-        const itemImageLocalPath = req.files?.image[0]?.path;
+      // ADD MENU ITEM TO DB
+      const menuItem = await menuCollection.create({
+        name: name,
+        description: desc,
+        image: image,
+        price: Number(price),
+        rating: 2.5,
+        userId: currentUser?._id,
+      });
 
-        // ADD FILE IN CLOUD
-        const itemImage = await uploadOnCloudinary(itemImageLocalPath);
+      // CHECK MENU ITEM IS CREATED OR NOT
+      const createdMenuItem = await menuCollection
+        .findById(menuItem._id)
+        .select("-createdAt -updatedAt -__v");
 
-        // CHECK IMAGE UPLOADED OR NOT
-        if (!itemImage) {
-          return res.json(
-            new ApiErrors({
-              statusCode: 404,
-              statusText: "IMAGE IS NOT UPLOADED IN CLOUD!",
-            })
-          );
-        }
-
-        // ADD MENU ITEM TO DB
-        const menuItem = await menuCollection.create({
-          name: name,
-          description: desc,
-          image: itemImage,
-          price: Number(price),
-          rating: 2.5,
-          userId: currentUser?._id,
-        });
-
-        // CHECK MENU ITEM IS CREATED OR NOT
-        const createdMenuItem = await menuCollection
-          .findById(menuItem._id)
-          .select("-createdAt -updatedAt -__v");
-
-        if (!createdMenuItem) {
-          return res.json(
-            new ApiErrors({
-              statusCode: 409,
-              statusText: "MENU ITEM NOT CREATED!",
-            })
-          );
-        }
-
-        // PUSH THE ITEMS TO USER DB
-        await userCollection.findByIdAndUpdate(
-          { _id: currentUser?._id },
-          {
-            $push: {
-              menuItems: createdMenuItem?._id,
-            },
-          }
-        );
-
-        // SEND RESPONSE OF MENUITEM
-        return res.json(
-          new ApiResponse({
-            statusCode: 201,
-            statusText: "MENU ITEM CREATED SUCCESSFULLY!",
-            data: createdMenuItem,
-          })
-        );
-      } else {
+      if (!createdMenuItem) {
         return res.json(
           new ApiErrors({
-            statusCode: 404,
-            statusText: "IMAGE IS REQUIRED!",
+            statusCode: 409,
+            statusText: "MENU ITEM CREATED FAILED!",
           })
         );
       }
+
+      // PUSH THE ITEMS TO USER DB
+      await userCollection.findByIdAndUpdate(
+        { _id: currentUser?._id },
+        {
+          $push: {
+            menuItems: createdMenuItem?._id,
+          },
+        }
+      );
+
+      // SEND RESPONSE OF MENUITEM
+      return res.json(
+        new ApiResponse({
+          statusCode: 201,
+          statusText: "MENU ITEM CREATED SUCCESSFULLY!",
+          data: createdMenuItem,
+        })
+      );
     } catch (err) {
-      console.error("ERROR IN CREATE MENU ITEM :", err);
+      console.error("ERROR IN CREATE MENU ITEM FAILED :", err);
       return res.json(
         new ApiErrors({
           statusCode: 400,
