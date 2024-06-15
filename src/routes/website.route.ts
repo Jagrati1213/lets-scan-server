262c42d5090c8c4db5248fac5ec5230ae26796a7
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-15 18:32:42 +0530

    JG: del

--- src/routes/website.route.ts
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getMenuItemsController } from "../controllers/website/getMenuItems.controller";
-
-export const menuWebsiteRouter = express.Router();
-
-menuWebsiteRouter.get("/:userId", getMenuItemsController);
