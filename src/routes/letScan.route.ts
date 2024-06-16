d5e9432b8abf5f0e7afdaf086955e3f2e46ba403
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-16 15:41:35 +0530

    JG: add id in api

--- src/routes/letScan.route.ts
+++ src/routes/letScan.route.ts
@@ -4,5 +4,5 @@ import { getAllVendors } from "../controllers/letsScan/getAllVendors.controller"
 
 export const letScanRouter = express.Router();
 
-letScanRouter.get("/menu-lists", getAllMenuItemsController);
+letScanRouter.get("/menu-lists/:userId", getAllMenuItemsController);
 letScanRouter.get("/all-venders", getAllVendors);
