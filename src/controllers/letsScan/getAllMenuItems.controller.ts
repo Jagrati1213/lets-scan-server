b2f3cb8d7d16b0a4ecf088e3bc0af25b9c18d444
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-17 13:06:28 +0530

    JG: return is open

--- src/controllers/letsScan/getAllMenuItems.controller.ts
+++ src/controllers/letsScan/getAllMenuItems.controller.ts
@@ -7,40 +7,52 @@ import mongoose from "mongoose";
 
 export const getAllMenuItemsController = asyncHandler(
   async (req: Request, res: Response) => {
-    // GET USER ID
-    const { userId } = req.params;
+    try {
+      // GET USER ID
+      const { userId } = req.params;
 
-    // VERIFY USER ID
-    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      // VERIFY USER ID
+      if (!mongoose.Types.ObjectId.isValid(userId)) {
+        return res.json(
+          new ApiErrors({
+            statusCode: 400,
+            statusText: "INVALID USER ID",
+          })
+        );
+      }
+
+      // CHECK USER
+      const currentUser = await userCollection.findById(userId).populate({
+        path: "menuItems",
+        match: { isActive: true },
+        select: "-createdAt -updatedAt -__v -userId -isActive",
+      });
+
+      if (!currentUser) {
+        return res.json(
+          new ApiErrors({
+            statusCode: 404,
+            statusText: "USER NOT FOUNDED!",
+          })
+        );
+      }
+      return res.json(
+        new ApiResponse({
+          statusCode: 200,
+          statusText: "ALL MENU LISTS",
+          data: {
+            menuItems: currentUser.menuItems,
+            isOpen: currentUser.isOpen,
+          },
+        })
+      );
+    } catch (error) {
       return res.json(
         new ApiErrors({
-          statusCode: 400,
-          statusText: "INVALID USER ID",
+          statusCode: 500,
+          statusText: "ERROR IN FETCH MENU LIST",
         })
       );
     }
-
-    // CHECK USER
-    const currentUser = await userCollection.findById(userId).populate({
-      path: "menuItems",
-      match: { isActive: true },
-      select: "-createdAt -updatedAt -__v -userId -isActive",
-    });
-
-    if (!currentUser) {
-      return res.json(
-        new ApiErrors({
-          statusCode: 404,
-          statusText: "USER NOT FOUNDED!",
-        })
-      );
-    }
-    return res.json(
-      new ApiResponse({
-        statusCode: 200,
-        statusText: "MENU LISTS",
-        data: currentUser.menuItems,
-      })
-    );
   }
 );
