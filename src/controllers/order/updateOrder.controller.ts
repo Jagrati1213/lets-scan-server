8d95cc2786ad421abb276f3837efd35f7ddd5cce
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-17 22:32:53 +0530

    JG: handle order details

---
+++ src/controllers/order/updateOrder.controller.ts
@@ -0,0 +1,139 @@
+import { Request, Response } from "express";
+import { asyncHandler } from "../../utils/asyncHandler";
+import mongoose from "mongoose";
+import { ApiErrors } from "../../utils/apiErrors";
+import { userCollection } from "../../models/user.model";
+import { generateOrderTokenAndCode } from "../token/generateOrderToken.controller";
+import { OrderCollection } from "../../models/order.module";
+import { ApiResponse } from "../../utils/apiResponse";
+import bcrypt from "bcrypt";
+import { paymentCollection } from "../../models/payment.model";
+
+export const updateOrderController = asyncHandler(
+  async (req: Request, res: Response) => {
+    try {
+      // GET ORDER DETAILS FROM REQUEST
+      const { name, email, orderList, paymentId, userId } = req.body;
+
+      // VERIFY USER ID
+      if (!mongoose.Types.ObjectId.isValid(userId)) {
+        return res.json(
+          new ApiErrors({
+            statusCode: 404,
+            statusText: "Invalid USER ID",
+          })
+        );
+      }
+
+      // VERIFY THE FIELDS
+      if (!name || !email || !orderList || !orderList?.length) {
+        return res.json(
+          new ApiErrors({
+            statusCode: 400,
+            statusText: "ALL FIELDS ARE REQUIRED!",
+          })
+        );
+      }
+
+      // VERIFY PAYMENT DETAILS
+      if (!paymentId) {
+        return res.json(
+          new ApiErrors({
+            statusCode: 400,
+            statusText: "PROVIDE PAYMENT DETAILS!",
+          })
+        );
+      }
+
+      //TODO: CHECK THAT EMAIL IS ALREADY PRESENT
+
+      // FIND CURRENT USER
+      const currentUser = await userCollection.findById(userId);
+
+      if (!currentUser) {
+        return res.json(
+          new ApiErrors({
+            statusCode: 404,
+            statusText: "USER NOT FOUNDED!",
+          })
+        );
+      }
+
+      // GENERATE TOKEN(6) & VERIFY CODE(4)
+      const { token, verifyCode } = generateOrderTokenAndCode({
+        tokenLength: 6,
+        codeLength: 4,
+      });
+
+      // BCRYPT VERIFY CODE
+      const bcryptVerifyCode = await bcrypt.hash(verifyCode, 4);
+
+      // PUSH THE ORDER DETAILS, TOKEN, & VERIFY CODE TO ORDER COLLECTION
+      const newOrder = await OrderCollection.create({
+        name: name,
+        email: email,
+        orderToken: token,
+        verifyCode: bcryptVerifyCode,
+        orderStatus: "pickup",
+        userId: currentUser._id,
+        orderList: orderList,
+      });
+
+      const updatedOrder = await OrderCollection.findByIdAndUpdate(
+        newOrder._id,
+        {
+          paymentId: paymentId,
+        },
+        { new: true }
+      );
+
+      // VERIFY ORDER
+      const createOrder = await OrderCollection.findById(updatedOrder?._id);
+
+      if (!createOrder) {
+        return res.json(
+          new ApiErrors({
+            statusCode: 400,
+            statusText: "ORDER NOT PLACED, TRY AGAIN!",
+          })
+        );
+      }
+
+      // PUSH ORDER TO USER COLLECTION
+      await userCollection.findByIdAndUpdate(
+        { _id: userId },
+        {
+          $push: {
+            orders: createOrder?._id,
+          },
+        },
+        {
+          new: true,
+        }
+      );
+
+      // PUSH ORDER-ID TO PAYMENT COLLECTION
+      await paymentCollection.findByIdAndUpdate(
+        paymentId,
+        { orderId: createOrder._id },
+        { new: true }
+      );
+
+      // RETURN TOKEN & VERIFY CODE
+      return res.json(
+        new ApiResponse({
+          statusCode: 201,
+          statusText: "ORDER PLACED SUCCESSFULLY!",
+          data: createOrder,
+        })
+      );
+    } catch (error) {
+      return res.json(
+        new ApiErrors({
+          statusCode: 400,
+          statusText: `ERROR IN INTERNAL!, ${error}`,
+        })
+      );
+    }
+  }
+);
