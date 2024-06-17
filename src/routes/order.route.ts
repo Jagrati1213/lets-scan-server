71435022bcfde5e7355d23b074ae125ac55584a6
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-17 22:33:35 +0530

    JG: add router

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { generateOrderIdController } from "../controllers/order/generateOrderId.controller";
-import { paymentController } from "../controllers/order/payment.controller";
+import { paymentVerifyController } from "../controllers/order/payment.controller";
 import { getRazorKey } from "../controllers/order/getRazorKey.controller";
+import { updateOrderController } from "../controllers/order/updateOrder.controller";
 
 export const orderRouter = express.Router();
 
 // CREATE ROUTER
 orderRouter.post("/checkout", generateOrderIdController);
-orderRouter.post("/payment-verify", paymentController);
+orderRouter.post("/payment-verify", paymentVerifyController);
 orderRouter.get("/razor-key", getRazorKey);
+orderRouter.post("/", updateOrderController);
