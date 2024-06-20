fa9da461958fae77d281d5a8979107bc096471ba
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-20 13:36:37 +0530

    JG: added order api

--- src/routes/letScan.route.ts
+++ src/routes/letScan.route.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { getAllMenuItemsController } from "../controllers/letsScan/getAllMenuItems.controller";
 import { getAllVendors } from "../controllers/letsScan/getAllVendors.controller";
+import { getOrderDetailsController } from "../controllers/letsScan/getOrderDetails.controller";
 
 export const letScanRouter = express.Router();
 
 letScanRouter.get("/menu-lists/:vendorId", getAllMenuItemsController);
 letScanRouter.get("/all-vendors", getAllVendors);
+letScanRouter.get("/all-orders/:vendorId ", getOrderDetailsController);
