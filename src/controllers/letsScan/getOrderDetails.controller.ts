f8ea35e1b89e3e9f92db89635390b38ff08425ef
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-22 01:10:25 +0530

    JG; Fixed user order details

--- src/controllers/letsScan/getOrderDetails.controller.ts
+++ src/controllers/letsScan/getOrderDetails.controller.ts
@@ -2,45 +2,44 @@ import { Request, Response } from "express";
 import { asyncHandler } from "../../utils/asyncHandler";
 import { ApiErrors } from "../../utils/apiErrors";
 import { ApiResponse } from "../../utils/apiResponse";
-import { vendorCollection } from "../../models/vendor.model";
 import mongoose from "mongoose";
+import { OrderCollection } from "../../models/order.module";
 
 export const getOrderDetailsController = asyncHandler(
   async (req: Request, res: Response) => {
     try {
       // GET VENDOR ID
-      const { vendorId } = req.params;
+      const { orderId } = req.params;
 
       // VERIFY ID
-      if (!mongoose.Types.ObjectId.isValid(vendorId)) {
+      if (!mongoose.Types.ObjectId.isValid(orderId)) {
         return res.json(
           new ApiErrors({
             statusCode: 400,
-            statusText: "INVALID VENDOR ID",
+            statusText: "INVALID ORDER ID",
           })
         );
       }
 
       // CHECK USER
-      const currentVendor = await vendorCollection.findById(vendorId).populate({
-        path: "orders",
-        select: "-paymentId -vendorId -createdAt -updatedAt -__v",
-      });
+      const userOrderDeTails = await OrderCollection.findById(orderId).select(
+        "-vendorId -createdAt -updatedAt -__v"
+      );
 
-      if (!currentVendor) {
+      if (!userOrderDeTails) {
         return res.json(
           new ApiErrors({
             statusCode: 404,
-            statusText: "VENDOR NOT FOUNDED!",
+            statusText: "USER'S ORDER DETAILS NOT FOUNDED!",
           })
         );
       }
       return res.json(
         new ApiResponse({
           statusCode: 200,
-          statusText: "ALL ORDERS LIST",
+          statusText: "ORDERS",
           data: {
-            orders: currentVendor.orders,
+            orderDetails: userOrderDeTails,
           },
         })
       );
