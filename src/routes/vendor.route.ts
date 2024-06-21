5b9a7f7877f8d80a64b4dd6db749425dadd1a1cf
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-21 21:01:04 +0530

    JG: import get order  controller

--- src/routes/vendor.route.ts
+++ src/routes/vendor.route.ts
@@ -6,6 +6,7 @@ import { logOutController } from "../controllers/auth/logout.controller";
 import { refreshTokenController } from "../controllers/token/refreshToken.controller";
 import { getVendorDetailsController } from "../controllers/vendor/getVendorDetails.controller";
 import { updateIsOpenController } from "../controllers/vendor/updateIsOpen.controller";
+import { getAllOrdersController } from "../controllers/vendorOrder/getAllOrders.controller";
 
 export const vendorRouter = express.Router();
 
@@ -16,3 +17,4 @@ vendorRouter.get("/logout", vendorVerify, logOutController);
 vendorRouter.get("/refresh-token", refreshTokenController);
 vendorRouter.get("/", vendorVerify, getVendorDetailsController);
 vendorRouter.put("/open-shop", vendorVerify, updateIsOpenController);
+vendorRouter.get("/order", vendorVerify, getAllOrdersController);
