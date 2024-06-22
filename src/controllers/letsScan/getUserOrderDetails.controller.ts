4fa55e91e09bff419056342e7a04d7d9074a749a
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-23 03:30:20 +0530

    JG: get user order details

---
+++ src/controllers/letsScan/getUserOrderDetails.controller.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from "express";
+import { asyncHandler } from "../../utils/asyncHandler";
+import { ApiErrors } from "../../utils/apiErrors";
+import { OrderCollection } from "../../models/order.module";
+import { vendorCollection } from "../../models/vendor.model";
+import { ApiResponse } from "../../utils/apiResponse";
+import mongoose from "mongoose";
+
+export const getUserOrderDetailsController = asyncHandler(
+  async (req: Request, res: Response) => {
+    try {
+      // GET ORDER ID
+      const { orderId } = req.params;
+
+      // VERIFY VENDER ID
+      if (!mongoose.Types.ObjectId.isValid(orderId)) {
+        return res.json(
+          new ApiErrors({
+            statusCode: 400,
+            statusText: "INVALID VENDER ID",
+          })
+        );
+      }
+
+      // CHECK ORDER & GET VENDOR NAME
+      const order = await OrderCollection.findById(orderId).populate(
+        "vendorId"
+      );
+
+      if (!order) {
+        return res.json(
+          new ApiErrors({ statusCode: 400, statusText: "ORDER NOT EXISTS!" })
+        );
+      }
+
+      // RETRIEVE VENDOR RESTAURANT
+      const vendor = await vendorCollection.findById(order.vendorId);
+
+      // IF VENDOR DOES NOT EXIST
+      if (!vendor) {
+        return res.json(
+          new ApiErrors({ statusCode: 404, statusText: "VENDOR NOT FOUNDED!" })
+        );
+      }
+
+      // RESPOND WITH ORDER DETAILS INCLUDING VENDOR RESTAURANT
+      res.json(
+        new ApiResponse({
+          statusCode: 200,
+          statusText: "ORDER DETAILS",
+          data: { orderDetails: order, restaurant: vendor.restaurant },
+        })
+      );
+    } catch (error) {
+      return res.json(
+        new ApiErrors({ statusCode: 400, statusText: "ERROR IN GET DETAILS!" })
+      );
+    }
+  }
+);
