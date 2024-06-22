397cf484e2b2f19a00adfa60f647394adecbf351
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-23 03:30:42 +0530

    JG: added router

--- src/routes/letScan.route.ts
+++ src/routes/letScan.route.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { getAllMenuItemsController } from "../controllers/letsScan/getAllMenuItems.controller";
 import { getAllVendors } from "../controllers/letsScan/getAllVendors.controller";
 import { getOrderDetailsController } from "../controllers/letsScan/getOrderDetails.controller";
+import { getUserOrderDetailsController } from "../controllers/letsScan/getUserOrderDetails.controller";
 
 export const letScanRouter = express.Router();
 
 letScanRouter.get("/menu-lists/:vendorId", getAllMenuItemsController);
 letScanRouter.get("/all-vendors", getAllVendors);
 letScanRouter.get("/all-orders/:vendorId ", getOrderDetailsController);
+letScanRouter.get("/order-details/:orderId ", getUserOrderDetailsController);
