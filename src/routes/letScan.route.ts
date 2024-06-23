0ca7bd7b4cfc1d6073d53585571b5bcbd6b5f86a
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-23 15:27:48 +0530

    JG: remove white space

--- src/routes/letScan.route.ts
+++ src/routes/letScan.route.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import { getAllMenuItemsController } from "../controllers/letsScan/getAllMenuItems.controller";
 import { getAllVendors } from "../controllers/letsScan/getAllVendors.controller";
-import { getOrderDetailsController } from "../controllers/letsScan/getOrderDetails.controller";
+// import { getOrderDetailsController } from "../controllers/letsScan/getOrderDetails.controller";
 import { getUserOrderDetailsController } from "../controllers/letsScan/getUserOrderDetails.controller";
 
 export const letScanRouter = express.Router();
 
 letScanRouter.get("/menu-lists/:vendorId", getAllMenuItemsController);
 letScanRouter.get("/all-vendors", getAllVendors);
-letScanRouter.get("/all-orders/:vendorId ", getOrderDetailsController);
-letScanRouter.get("/order-details/:orderId ", getUserOrderDetailsController);
+// letScanRouter.get("/all-orders/:orderId ", getOrderDetailsController);
+letScanRouter.get("/order-details/:orderId", getUserOrderDetailsController);
