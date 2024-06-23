975f57429bab967bbbb1407084aefd3015733819
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-24 02:46:23 +0530

    JG: added Total values in vendor data

--- src/controllers/vendor/getVendorDetails.controller.ts
+++ src/controllers/vendor/getVendorDetails.controller.ts
@@ -1,30 +1,94 @@
 import { Response } from "express";
 import { asyncHandler } from "../../utils/asyncHandler";
 import { vendorCollection } from "../../models/vendor.model";
+import { OrderCollection } from "../../models/order.module";
+import { menuCollection } from "../../models/menu.model";
 import { ApiErrors } from "../../utils/apiErrors";
 import { ApiResponse } from "../../utils/apiResponse";
 import { CustomRequestT } from "../../types";
+import mongoose from "mongoose";
 
 export const getVendorDetailsController = asyncHandler(
   async (req: CustomRequestT, res: Response) => {
     try {
-      const currentVender = await vendorCollection
+      let totalRevenue: number = 0;
+      let totalOrders: number = 0;
+      let totalCustomers: number = 0;
+      let item: string | null | undefined = "unKnown";
+
+      // CHECK VENDOR
+      const currentVendor = await vendorCollection
         .findById(req.vendor?._id)
         .select(
           "-password -refreshToken -createdAt -updatedAt -__v -menuItems -orders"
         );
-      if (!currentVender)
+
+      if (!currentVendor)
         return res.json(
           new ApiErrors({
             statusCode: 400,
             statusText: "VENDOR NOT FOUNDED!",
           })
         );
+
+      // GET ORDERS
+      const orders = await OrderCollection.find({
+        vendorId: currentVendor?._id,
+      });
+
+      // IF ORDER HAVE
+      if (orders) {
+        // CALCULATE ORDERS
+        totalOrders = orders.length;
+
+        // CALCULATE REVENUE
+        totalRevenue = orders.reduce(
+          (sum, order) => sum + (order.totalAmount || 0),
+          0
+        );
+
+        // CALCULATE UNIQUE CUSTOMERS
+        const uniqueCustomerEmails = new Set();
+        orders.forEach((order) =>
+          uniqueCustomerEmails.add(order?.customer?.email)
+        );
+        totalCustomers = uniqueCustomerEmails.size;
+      }
+
+      // CALCULATE BEST SELLING ITEM
+      const bestSellingItem = await OrderCollection.aggregate([
+        {
+          $match: { vendorId: new mongoose.Types.ObjectId(currentVendor?._id) },
+        },
+        { $unwind: "$orderList" },
+        {
+          $group: {
+            _id: "$orderList.menuId",
+            totalQuantity: { $sum: "$orderList.quantity" },
+          },
+        },
+        { $sort: { totalQuantity: -1 } },
+        { $limit: 1 },
+      ]);
+      const bestMenuItem = await menuCollection.findById(
+        bestSellingItem[0]._id
+      );
+      if (!bestSellingItem || !bestMenuItem) {
+        item = "No Best Selling Item";
+      } else {
+        item = bestMenuItem.name;
+      }
       return res.json(
         new ApiResponse({
           statusCode: 200,
           statusText: "VENDOR EXITS",
-          data: currentVender,
+          data: {
+            ...currentVendor.toObject(),
+            totalCustomers,
+            totalOrders,
+            totalRevenue,
+            bestSell: item,
+          },
         })
       );
     } catch (error) {
